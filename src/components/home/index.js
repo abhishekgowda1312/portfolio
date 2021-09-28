7d8008d42b7045397e2ce690d8bb8f05816d219f
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-09-28 22:04:46 +0530

    home component complete

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Body from '../body'
+import Footer from '../footer'
+import Header from '../header'
 import './home.css'
 
 function Home() {
     return (
-        <div>
-            This is the home page
+        <div className="home">
+            <Header />
+            <Body />
+            <Footer />
         </div>
     )
 }
