ad382182b0c2bf2e5f19cf086749944cc796cd0e
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-09-29 12:01:29 +0530

    About section 1 complete

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './About.css'
 
 function About() {
     return (
-        <div>
-            About
+        <div className="about">
+            <div className="about-top">
+                <div className="about-info">
+                    Hey there 👋, I'm <span className="info-name">Abhishek.</span> <br /> I'm a Front End Developer, who loves to code!<br />
+                    Currently working on React, Javascript & building clones of websites.
+                </div>
+                <div className="about-photo">
+                    <img className="about-image" height="200px" width="200px" src="https://avatars.githubusercontent.com/u/73712257?v=4" alt="Abhishek" />
+                </div>
+            </div>
+            <div className="about-bottom">
+                Contacts
+            </div>
         </div>
     )
 }
