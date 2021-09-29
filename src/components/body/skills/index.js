61acb485a8e8a29389b302e74d44083941c933c5
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-09-29 14:39:32 +0530

    Skills component complete

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Skills.css'
+import Separator from '../../common/separator/index'
+import { SkillsData } from '../../data/skills'
+import SkillCard from './skill-card'
 
 function Skills() {
+    const skills = SkillsData
+
+
     return (
-        <div>
-            skills
+        <div className="skills">
+            <Separator />
+            <label className="section-title">Skills</label>
+            <div className="skills-container">
+                {skills.map((item) => {
+                    return (
+                        <div className="skills-section" key={item.type}>
+                            <label className="skills-section-title">{item.type}</label>
+                            <div className="skills-list">
+                                {item.list.map(skill => {
+                                    return (
+                                        <SkillCard skill={skill} key={skill.name} />
+
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/components/data/skills.js
@@ -0,0 +1,40 @@
+export const SkillsData = [
+    {
+        type: "FrontEnd",
+        list: [
+            {
+                name: "ReactJS",
+                icon: <i className="devicon-react-original colored"></i>
+            },
+            {
+                name: "JavaScript",
+                icon: <i className="devicon-javascript-plain colored"></i>
+            },
+            {
+                name: "HTML",
+                icon: <i className="devicon-html5-plain colored"></i>
+            },
+            {
+                name: "CSS",
+                icon: <i className="devicon-css3-plain-wordmark colored"></i>
+            },
+            {
+                name: "NextJS",
+                icon: <i className="devicon-nextjs-original colored"></i>
+            },
+        ]
+    },
+    {
+        type: "BackEnd",
+        list: [
+            {
+                name: "FireBase",
+                icon: <i className="devicon-firebase-plain colored"></i>
+            },
+            {
+                name: "NodeJS",
+                icon: <i className="devicon-nodejs-plain colored"></i>
+            },
+        ]
+    }
+]
